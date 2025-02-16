ec844528c215cc9a337f7c2f529b7d200d0347ef
Sudhanshu Singh <146207926+sudhaanshu94@users.noreply.github.com>
2025-02-16 17:19:34 +0530

    copy -> copied

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 function App() {
   const [length, setLength] = useState(8);
   const [numAll, setNumAll] = useState(false);
+  const [sudh, setSudh] = useState(false);
   const [charAll, setCharAll] = useState(false);
   const [password, setPassword] = useState("")
+  const [copied, setCopied] = useState("Copy")
 
   const passwordRef = useRef(null)
 
@@ -19,24 +21,32 @@ function App() {
       let char = Math.floor(Math.random() * str.length);
       pass += str.charAt(char);
     }
-
+    if(length==15 && sudh==true)pass="SUDHANSHU SINGH";
     setPassword(pass);
-  }, [length, numAll, charAll, setPassword])
+    setCopied("Copy");
+  }, [length, sudh, numAll, charAll, setPassword])
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
     passwordRef.current?.setSelectionRange(0, 999);
-    window.navigator.clipboard.writeText(password)
+    window.navigator.clipboard.writeText(password);
+    setCopied("Copied");
+
   }, [password])
 
   useEffect(() => {
     passwordGenerator()
-  }, [length, numAll, charAll, passwordGenerator])
+  }, [length, sudh, numAll, charAll, passwordGenerator])
 
   return (
     <div className="mt-50 flex justify-center items-center">
     <div className="w-auto h-auto mx-auto shadow-lg rounded-xl px-6 py-5 my-12 bg-gray-900 text-orange-500">
-      <h1 className='text-white text-center font-bold text-3xl mb-10 rounded-xl px-2 py-2'>Password Generator</h1>
+      <h1 className='text-white text-center underline font-bold text-3xl mb-10 rounded-xl px-2 py-1'>Password Generator
+      <p className='text-xs font-normal mt-3 flex justify-center items-center'>SUDHANSHU SINGH at Length: 15 
+        <input className='ml-2 mt-0.5 w-3 h-3' type='checkbox' checked={sudh} onChange={() => setSudh((sudh) => !sudh)}></input></p>
+      
+      </h1>
+      
       <div className="flex shadow-lg rounded-lg overflow-hidden mb-5">
         <input
           type="text"
@@ -50,7 +60,7 @@ function App() {
         onClick={copyPasswordToClipboard}
         className="bg-blue-600 hover:bg-gray-200 hover:text-black text-white px-4 py-2 rounded-r-lg"
       >
-        Copy
+        {copied}
       </button>
       
       </div>
@@ -60,7 +70,7 @@ function App() {
           <input
             type="range"
             min={6}
-            max={32}
+            max={50}
             value={length}
             className='cursor-pointer w-2/3 accent-orange-500'
             onChange={(e) => setLength(Number(e.target.value))}
