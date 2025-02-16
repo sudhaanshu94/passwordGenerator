25be7a850693c467fadb9c421dfc3729dd14f7d3
Sudhanshu Singh <146207926+sudhaanshu94@users.noreply.github.com>
2025-02-16 17:05:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/passwordGenerator/",  // Ensure this matches your GitHub repo name
-  plugins: [react() , tailwindcss()]
+  base: "/passwordGenerator/", // Ensure this matches your GitHub repo name
+  plugins: [react()]
 })
